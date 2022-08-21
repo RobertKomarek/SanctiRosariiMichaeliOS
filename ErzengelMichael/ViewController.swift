563ebf67e862fa2aa3a07458c9a72b912506181c
marinakomarek <robert.komarek@gmx.de>
2022-08-21 23:34:08 +0200

    Initial Commit

---
+++ ErzengelMichael/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErzengelMichael
+//
+//  Created by Marina Komarek on 21.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
