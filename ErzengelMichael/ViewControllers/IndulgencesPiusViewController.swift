12094a895797e29a040d2c623cafcf1649045919
Robert Komarek <robert.komarek98@gmail.com>
2023-11-18 21:00:16 +0100

    Sa. 18. Nov. 21:00

--- ErzengelMichael/ViewControllers/IndulgencesPiusViewController.swift
+++ ErzengelMichael/ViewControllers/IndulgencesPiusViewController.swift
@@ -1,10 +1,3 @@
-//
-//  IndulgencesPiusViewController.swift
-//  ErzengelMichael
-//
-//  Created by Marina Komarek on 19.11.22.
-//
-
 import UIKit
 
 class IndulgencesPiusViewController: UIViewController {
@@ -35,49 +28,12 @@ class IndulgencesPiusViewController: UIViewController {
         //Load Json file with app details
         arrayAppDetails = appDetails.getAppDetails(jsonName: "SanctiRosariiMichael", language: language ?? "English")
         
-//        switch language {
-//            //Check chosen language Deutsch, Englisch, Español, Italiano, Português, Français
-//        case "Deutsch":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Versprechungen"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        case "English":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Promises"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        case "Español":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Promesas"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        case "Italiano":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Promesse"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        case "Português":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Promessas"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        case "Français":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Promesses"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        case "中文":
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "承诺"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        default:
-//            let backButton = UIBarButtonItem()
-//            backButton.title = "Promises"
-//            backButton.tintColor = .white
-//            self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-//        }
+        let backButton = UIBarButtonItem()
+        backButton.title = arrayAppDetails[0].TabBarPromises
+        backButton.tintColor = .white
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        
+ 
         
         self.navigationItem.title = arrayAppDetails[0].TitlePromisesIndulgences
         textViewIndulgences.text = arrayAppDetails[0].PromisesIndulgences
